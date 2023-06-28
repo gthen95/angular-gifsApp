b3f28513aebc9c347f0c1fdfb674f5b4e7072096
Gerald Then <gthen@mdsoft.com.do>
2023-06-28 14:18:43 -0400

    Correcion tamano gifs

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  baseUrl: "https://gthen95.github.io/angular-gifsApp"
+  baseUrl: "https://gthen95.github.io/angular-gifsApp",
+  TestUrl: "http://localhost:4200"
 };
